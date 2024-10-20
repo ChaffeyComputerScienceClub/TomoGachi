ddabf2a25f947025f69a4652c4466dbe7c583cd8
Thlipsi <121302871+Thlipsi@users.noreply.github.com>
2024-10-20 13:25:05 -0700

    Update loadManager.js
    setting up the ability to toggle the extended mini sidebar

--- loadManager.js
+++ loadManager.js
@@ -29,14 +29,14 @@ const mini_option4 = [
 
 options.forEach(option => {
     option.addEventListener('click', function() {
-        // Remove active class from all options
-        options.forEach(opt => opt.classList.remove('active'));
-        this.classList.add('active');
-        document.body.style.backgroundColor = getComputedStyle(this).backgroundColor;
         if (this.id == activeOption){
             console.log('already selected');
+            options.forEach(opt => opt.classList.remove('active'));
+            activeOption = null;
         }
         else {
+            this.classList.add('active');
+            document.body.style.backgroundColor = getComputedStyle(this).backgroundColor;
             activeOption = this.id;
             if (this.id == 'option1'){
                 clearScreen();
