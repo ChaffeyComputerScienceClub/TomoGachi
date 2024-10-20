c82e1796660e6fb247ee26c29e85bd95bfbda5aa
Thlipsi <121302871+Thlipsi@users.noreply.github.com>
2024-10-20 13:43:56 -0700

    smoothing transitions and loading
    mini sidebar is now able to be toggled on and off, screen only loads page if it is a new page

--- loadManager.js
+++ loadManager.js
@@ -1,5 +1,7 @@
 const options = document.querySelectorAll('.sidebar-option');
 let activeOption = null;
+let currentScreen = null;
+let switchScreen = false;
 
 const mini_option1 = [
     "1test1",
@@ -29,33 +31,48 @@ const mini_option4 = [
 
 options.forEach(option => {
     option.addEventListener('click', function() {
-        if (this.id == activeOption){
-            console.log('already selected');
-            options.forEach(opt => opt.classList.remove('active'));
-            activeOption = null;
+    options.forEach(opt => opt.classList.remove('active'));
+    if (this.id == activeOption){
+        clearSideBar();
+        activeOption = null;
+
+    }
+    else {
+        this.classList.add('active');
+        document.body.style.backgroundColor = getComputedStyle(this).backgroundColor;
+        activeOption = this.id;
+        if (currentScreen != this.id){
+            currentScreen = this.id;
+            switchScreen = true;
+            console.log(currentScreen);
         }
-        else {
-            this.classList.add('active');
-            document.body.style.backgroundColor = getComputedStyle(this).backgroundColor;
-            activeOption = this.id;
-            if (this.id == 'option1'){
+        if (this.id == 'option1'){
+            clearSideBar();
+            appendMiniSidebar(this.id);
+            if (switchScreen) {
                 clearScreen();
-                appendMiniSidebar(this.id);
                 shopLoadProducts();
             }
-            else if(this.id == 'option2') {
+        }
+        else if(this.id == 'option2') {
+            clearSideBar();
+            appendMiniSidebar(this.id);
+            if (switchScreen) {
                 clearScreen();
-                appendMiniSidebar(this.id);
                 invLoadInventory();
             }
-            else if(this.id == 'option3') {
-                clearScreen();
-                appendMiniSidebar(this.id);
-            }
-            else if(this.id == 'option4') {
-                clearScreen();
-                appendMiniSidebar(this.id);
-            }
+        }
+        else if(this.id == 'option3') {
+            clearScreen();
+            clearSideBar();
+            appendMiniSidebar(this.id);
+        }
+        else if(this.id == 'option4') {
+            clearScreen();
+            clearSideBar();
+            appendMiniSidebar(this.id);
+        }   
+        switchScreen = false;
     }
 });
 });
@@ -63,11 +80,17 @@ options.forEach(option => {
 function clearScreen() {
     const shop_grid = document.getElementById("shop-grid");
     shop_grid.innerHTML = "";
+ 
+}
+
+function clearSideBar() {
     const mini_sidebar = document.getElementById("mini-sidebar");
     mini_sidebar.innerHTML = "";
+    mini_sidebar.style.opacity = 0;
 }
 
 function appendMiniSidebar(option) {
+    const mini_sidebar = document.getElementById("mini-sidebar");
     let mini_option = null;
     if (option === "option1"){
         mini_option = mini_option1;
@@ -81,7 +104,7 @@ function appendMiniSidebar(option) {
     else if (option === "option4"){
         mini_option = mini_option4;
     }
-
+    mini_sidebar.style.opacity = 1;
     mini_option.forEach(item => {
         let mini_sidebar = document.getElementById('mini-sidebar');
         let mini_option = document.createElement("div");
