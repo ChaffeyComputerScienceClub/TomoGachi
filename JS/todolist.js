de715c65933aa1fc79f7ef8d94a8fbaedd2de3b6
Thlipsi <121302871+Thlipsi@users.noreply.github.com>
2024-11-26 23:56:43 -0800

    loads page
    kinda ugly, might wanna be less selfish with the uh container types hun. not everyone wants their buttons to look like that <3

--- JS/todolist.js
+++ JS/todolist.js
@@ -1,62 +1,66 @@
-var addButton=document.getElementById("addButton");
-addButton.addEventListener('click', NewItem);
-
-var ITEM_AMOUNT=0;
+var ITEM_AMOUNT=1;
 
 function CreateItem(inputText){
-  var input = document.createElement("input");
-  input.type="text";
-  var div=document.getElementById("div");
-  input.placeholder="untitled";
-  var br=document.createElement("<br>");
-  document.body.appendChild(br);
-  
-  // var num=2;
-  // input.id="tb"+num.toString();
-  
-// set(ref(db, "User/" + user.displayName + "/toDoList/"),{
-    
-// })
-}
-
-var div=document.getElementById("div");
-var ITEM_AMOUNT=0;
-
-function NewItem(){
   var user=auth.currentUser;
-  const clone=div.cloneNode(true);
-  document.body.appendChild(clone);
-  clone.style.visibility="visible";
-  ITEM_AMOUNT +=1;
-  set(ref(db, "User/" + user.displayName + "/toDoList/" + "/itemAmount/"),{
-    amount:ITEM_AMOUNT
+  const container = document.getElementById("shop-grid");
+  const toDoDiv = document.createElement('div'); 
+  toDoDiv.style ="display: flex; margin-left: 1rem;";
+  toDoDiv.id = "todoDiv";
+  toDoDiv.innerHTML = `
+    <input type="checkbox" id="cb" class="cbStyle">
+    <input type="text" value=${inputText} id=${ITEM_AMOUNT} class="tb">
+    `;
+  
+  container.appendChild(toDoDiv);
+  const elements = document.querySelectorAll(".tb");
+  
+  elements.forEach((element) => {
+    element.addEventListener('focusout', () => {
+      console.log("test");
+      console.log(element.id, element.innerHTML);
+      save(element.id, element.value);
+    })
+
+    // element.addEventListener('change', () => {
+    //   console.log("this is checked");
+    //   console.log(element.id, element.innerHTML);
+    //   deleteItem(element.id, element.value);
+    // })
+
   })
+  set(ref(db, "User/" + "lizzymak" + "/toDoList/" + ITEM_AMOUNT),{
+    Item: inputText
+  })
+  ITEM_AMOUNT += 1;
 }
 
+function deleteItem(id, text){
+  console.log("deleted");
+}
 
-var tb=document.getElementById("tb");
-tb.addEventListener('focusout', save);
-
-function save(){
+function save(id, text){
+  console.log("saving");
   var user=auth.currentUser;
-  var name=event.target.id;
-  console.log(name);
-  var item=event.target.value;
-  console.log(item);
-  set(ref(db, "User/" + user.displayName + "/toDoList/" + name),{
-    Item:item
+  set(ref(db, "User/" + "lizzymak" + "/toDoList/" + id),{
+    Item: text
   })
 }
 
 
 
 function todoPageLoad() {
+  const container = document.getElementById("shop-grid");
+  const addButton = document.createElement('button'); 
+  addButton.addEventListener('click', () => {
+    CreateItem("Untitled");
+  });
+  container.append(addButton);
   var user=auth.currentUser;
-  get(ref(db, "User/" + user.displayName + "/toDoList/" + name))
+  get(ref(db, "User/" + "lizzymak" + "/toDoList/"))
   .then((snapshot) => {
     if (snapshot.exists()){
-      for(let i = 0; i < Object.keys(snapshot.val()).length-1; i++) {
-        CreateItem(snapshot.val());
+      for(let i = 1; i <= Object.keys(snapshot.val()).length; i++) {
+        CreateItem(snapshot.child(i.toString()).val().Item);
       }
       return true;
     }
@@ -65,10 +69,12 @@ function todoPageLoad() {
     console.log(error);
   });
 
+
+ 
 };
 
 onAuthStateChanged(auth, (user)=>{
-  if(user){
+  if(true){
     console.log(user);
     PageLoad();
   }else{
